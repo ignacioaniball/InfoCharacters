ca79017934662f787bb7328fb53ddaafd9c1b692
ignacioaniball <ignacioaniball@gmail.com>
2018-11-14 16:58:58 -0300

    Base update

--- InfoCharactersApplication.java
+++ InfoCharactersApplication.java
@@ -2,11 +2,16 @@ package com.DotaCharacter.InfoCharacters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class InfoCharactersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InfoCharactersApplication.class, args);
+
+	    // Tell Boot to look for registration-server.yml
+        System.setProperty("spring.config.name", "registration-server");
+	    SpringApplication.run(InfoCharactersApplication.class, args);
 	}
 }
