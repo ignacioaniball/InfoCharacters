512b9233e6676a1c18d0e39bec473bd53b3ca8a2
ignacioaniball <ignacioaniball@gmail.com>
2018-11-14 15:36:26 -0300

    First commit

---
+++ InfoCharactersApplication.java
@@ -0,0 +1,12 @@
+package com.DotaCharacter.InfoCharacters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoCharactersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoCharactersApplication.class, args);
+	}
+}
